c42c30c9008e3b620cdd3204cdab5ca42e5fc77f
Corentin Girard <corentingirard.dev@gmail.com>
2020-07-07 14:39:48 +0200

    Created a helpers file

---
+++ src/helpers.ts
@@ -0,0 +1,12 @@
+export function splitBy<T>(array: T[], key: keyof T): T[][] {
+    const obj = Object();
+
+    for (const value of array) {
+        if (!obj[value[key]])
+            obj[value[key]] = [];
+
+        obj[value[key]].push(value);
+    }
+
+    return Object.values(obj);
+}
