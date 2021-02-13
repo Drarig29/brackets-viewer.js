15369b1aefe239e1d560044da42ce91b423b4f8c
Corentin Girard <corentingirard.dev@gmail.com>
2021-02-13 15:25:18 +0100

    Replace strings by i18n resources

--- src/lang.ts
+++ src/lang.ts
@@ -4,7 +4,7 @@ import { locales } from './i18n';
 
 import { Status } from 'brackets-model';
 import { isMajorRound } from './helpers';
-import { Locales, FinalType, BracketType, OriginHint, RankingHeaders } from './types';
+import { Locales, FinalType, BracketType, OriginHint, RankingHeaders, RankingHeader } from './types';
 
 type Locale = typeof locales['en'];
 
@@ -54,28 +54,28 @@ function i18n<Scope extends keyof Locale>(scope: Scope, key: keyof Locale[Scope]
 export function getOriginHint(roundNumber: number, roundCount: number, skipFirstRound: boolean, matchLocation: BracketType): OriginHint {
     if (roundNumber === 1) {
         if (matchLocation === 'single-bracket')
-            return (position: number): string => `Seed ${position}`;
+            return (position: number): string => i18n('origin-hint', 'seed', { position });
 
         if (matchLocation === 'winner-bracket')
-            return (position: number): string => `Seed ${position}`;
+            return (position: number): string => i18n('origin-hint', 'seed', { position });
 
         if (matchLocation === 'loser-bracket' && skipFirstRound)
-            return (position: number): string => `Seed ${position}`;
+            return (position: number): string => i18n('origin-hint', 'seed', { position });
     }
 
     if (isMajorRound(roundNumber) && matchLocation === 'loser-bracket') {
         if (roundNumber === roundCount - 2)
-            return (position: number): string => `Loser of WB Semi ${position}`;
+            return (position: number): string => i18n('origin-hint', 'winner-bracket-semi-final', { position });
 
         if (roundNumber === roundCount)
-            return (): string => 'Loser of WB Final';
+            return (): string => i18n('origin-hint', 'winner-bracket-final');
 
         const roundNumberWB = Math.ceil((roundNumber + 1) / 2);
 
         if (skipFirstRound)
-            return (position: number): string => `Loser of WB ${roundNumberWB - 1}.${position}`;
+            return (position: number): string => i18n('origin-hint', 'winner-bracket', { round: roundNumberWB - 1, position });
 
-        return (position: number): string => `Loser of WB ${roundNumberWB}.${position}`;
+        return (position: number): string => i18n('origin-hint', 'winner-bracket', { round: roundNumberWB, position });
     }
 
     return undefined;
@@ -90,27 +90,27 @@ export function getOriginHint(roundNumber: number, roundCount: number, skipFirst
  * @param matchLocation Location of the match.
  */
 export function getMatchLabel(matchNumber: number, roundNumber: number, roundCount: number, matchLocation: BracketType): string {
-    const matchPrefix = matchLocation === 'winner-bracket' ? 'WB' :
-        matchLocation === 'loser-bracket' ? 'LB' : 'M';
+    const matchPrefix = matchLocation === 'winner-bracket' ? i18n('match-label', 'winner-bracket') :
+        matchLocation === 'loser-bracket' ? i18n('match-label', 'loser-bracket') : i18n('match-label', "standard-bracket");
 
     const inSemiFinalRound = roundNumber === roundCount - 1;
     const inFinalRound = roundNumber === roundCount;
 
     if (matchLocation === 'single-bracket') {
         if (inSemiFinalRound)
-            return `Semi ${matchNumber}`;
+            return i18n('match-label', 'standard-bracket-semi-final', { matchNumber });
 
         if (inFinalRound)
-            return 'Final';
+            return i18n('match-label', 'standard-bracket-final');
     }
 
     if (inSemiFinalRound)
-        return `${matchPrefix} Semi ${matchNumber}`;
+        return i18n('match-label', 'double-elimination-semi-final', { matchPrefix, matchNumber });
 
     if (inFinalRound)
-        return `${matchPrefix} Final`;
+        return i18n('match-label', 'double-elimination-final', { matchPrefix });
 
-    return `${matchPrefix} ${roundNumber}.${matchNumber}`;
+    return i18n('match-label', 'double-elimination', { matchPrefix, roundNumber, matchNumber });
 }
 
 /**
@@ -123,7 +123,7 @@ export function getMatchLabel(matchNumber: number, roundNumber: number, roundCou
 export function getFinalMatchLabel(finalType: FinalType, roundNumber: number, roundCount: number): string {
     // Single elimination.
     if (finalType === 'consolation_final')
-        return 'Consolation Final';
+        return i18n('match-label', 'consolation-final');
 
     // Double elimination.
     return getGrandFinalName(roundNumber, roundCount);
@@ -138,12 +138,13 @@ export function getFinalMatchLabel(finalType: FinalType, roundNumber: number, ro
 export function getFinalOriginHint(finalType: FinalType, roundNumber: number): OriginHint {
     // Single elimination.
     if (finalType === 'consolation_final')
-        return (position: number): string => `Loser of Semi ${position}`;
+        return (position: number): string => i18n('origin-hint', 'consolation-final', { position });
 
     // Double elimination.
-    if (roundNumber === 1)
-        return (): string => 'Winner of LB Final';
+    if (roundNumber === 1) // Grand Final round 1
+        return (): string => i18n('origin-hint', 'grand-final');
 
+    // Grand Final round 2 (no hint because it's obvious both participants come from the previous round)
     return undefined;
 }
 
@@ -155,17 +156,17 @@ export function getFinalOriginHint(finalType: FinalType, roundNumber: number): O
 export function getMatchStatus(status: Status): string {
     switch (status) {
         case Status.Locked:
-            return 'Locked';
+            return i18n('match-status', 'locked');
         case Status.Waiting:
-            return 'Waiting';
+            return i18n('match-status', 'waiting');
         case Status.Ready:
-            return 'Ready';
+            return i18n('match-status', 'ready');
         case Status.Running:
-            return 'Running';
+            return i18n('match-status', 'running');
         case Status.Completed:
-            return 'Completed';
+            return i18n('match-status', 'completed');
         case Status.Archived:
-            return 'Archived';
+            return i18n('match-status', 'archived');
     }
 }
 
@@ -176,10 +177,11 @@ export function getMatchStatus(status: Status): string {
  * @param roundCount Count of final rounds.
  */
 export function getGrandFinalName(roundNumber: number, roundCount: number): string {
+    // Double elimination.
     if (roundCount === 1)
-        return 'Grand Final';
+        return i18n('match-label', 'grand-final-single');
 
-    return `GF Round ${roundNumber}`;
+    return i18n('match-label', 'grand-final', { roundNumber });
 }
 
 /**
@@ -188,7 +190,7 @@ export function getGrandFinalName(roundNumber: number, roundCount: number): stri
  * @param groupNumber Number of the group.
  */
 export function getGroupName(groupNumber: number): string {
-    return `Group ${groupNumber}`;
+    return i18n('common', 'group-name', { groupNumber });
 }
 
 /**
@@ -198,7 +200,7 @@ export function getGroupName(groupNumber: number): string {
  * @param roundCount Count of rounds.
  */
 export function getRoundName(roundNumber: number, roundCount: number): string {
-    return roundNumber === roundCount ? 'Final Round' : `Round ${roundNumber}`;
+    return roundNumber === roundCount ? i18n('common', 'round-name-final') : i18n('common', 'round-name', { roundNumber });
 }
 
 /**
@@ -208,7 +210,7 @@ export function getRoundName(roundNumber: number, roundCount: number): string {
  * @param roundCount Count of rounds.
  */
 export function getWinnerBracketRoundName(roundNumber: number, roundCount: number): string {
-    return roundNumber === roundCount ? 'WB Final Round' : `WB Round ${i18next.t('test')} ${roundNumber}`;
+    return roundNumber === roundCount ? i18n('common', 'round-name-winner-bracket-final') : i18n('common', 'round-name-winner-bracket', { roundNumber });
 }
 
 /**
@@ -218,71 +220,38 @@ export function getWinnerBracketRoundName(roundNumber: number, roundCount: numbe
  * @param roundCount Count of rounds.
  */
 export function getLoserBracketRoundName(roundNumber: number, roundCount: number): string {
-    return roundNumber === roundCount ? 'LB Final Round' : `LB Round ${roundNumber}`;
+    return roundNumber === roundCount ? i18n('common', 'round-name-loser-bracket-final') : i18n('common', 'round-name-loser-bracket', { roundNumber });
 }
 
 /**
  * Abbreviations used in the viewer.
  */
 export const abbreviations = {
-    win: 'W',
-    loss: 'L',
-    forfeit: 'F',
-    position: 'P',
-    seed: '#',
+    win: i18n('abbreviations', 'win'),
+    loss: i18n('abbreviations', 'loss'),
+    forfeit: i18n('abbreviations', 'forfeit'),
+    position: i18n('abbreviations', 'position'),
+    seed: i18n('abbreviations', 'seed'),
 };
 
 /**
  * The BYE literal.
  */
-export const BYE = 'BYE';
+export const BYE = i18n('common', 'bye');
 
 /**
  * Headers of the ranking.
  */
 export const headers: RankingHeaders = {
-    rank: {
-        text: '#',
-        tooltip: 'Rank',
-    },
-    id: {
-        text: 'Name',
-        tooltip: 'Name',
-    },
-    played: {
-        text: 'P',
-        tooltip: 'Played',
-    },
-    wins: {
-        text: abbreviations.win,
-        tooltip: 'Wins',
-    },
-    draws: {
-        text: 'D',
-        tooltip: 'Draws',
-    },
-    losses: {
-        text: abbreviations.loss,
-        tooltip: 'Losses',
-    },
-    forfeits: {
-        text: abbreviations.forfeit,
-        tooltip: 'Forfeits',
-    },
-    scoreFor: {
-        text: 'SF',
-        tooltip: 'Score For',
-    },
-    scoreAgainst: {
-        text: 'SA',
-        tooltip: 'Score Against',
-    },
-    scoreDifference: {
-        text: '+/-',
-        tooltip: 'Score Difference',
-    },
-    points: {
-        text: 'Pts',
-        tooltip: 'Points',
-    },
+    rank: i18n('ranking', 'rank', true) as RankingHeader,
+    id: i18n('ranking', 'id', true) as RankingHeader,
+    played: i18n('ranking', 'played', true) as RankingHeader,
+    wins: i18n('ranking', 'wins', true) as RankingHeader,
+    draws: i18n('ranking', 'draws', true) as RankingHeader,
+    losses: i18n('ranking', 'losses', true) as RankingHeader,
+    forfeits: i18n('ranking', 'forfeits', true) as RankingHeader,
+    scoreFor: i18n('ranking', 'score-for', true) as RankingHeader,
+    scoreAgainst: i18n('ranking', 'score-against', true) as RankingHeader,
+    scoreDifference: i18n('ranking', 'score-difference', true) as RankingHeader,
+    points: i18n('ranking', 'points', true) as RankingHeader,
 };
